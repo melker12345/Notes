cbdc06ce3ce900c19a1f9438820e46f676ce9a58
Melker <memleoberg@gmail.com>
2025-11-09 23:31:09 +0100

    auto commit [#57]

--- code/code/sandbox/main.c
+++ code/code/sandbox/main.c
@@ -22,9 +22,10 @@ int main() {
       0b10000001, 0b10000001, 0b10000001, 0b00000000,
   };
 
+  // Move FONTS into a enum and then loop through all.
   for (int i = 0; i < 2; i++) {
     for (int y = 0; y < CHARACTER_HEIGHT; y++) {
-      int row_pattern = FONT_B[y];
+      int row_pattern = FONT_A[y];
 
       for (int x = 0; x < CHARACTER_WIDTH; x++) {
         int bit_mask = 1 << (CHARACTER_WIDTH - 1 - x);
