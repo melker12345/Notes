63fa91b0a0a5768a1262bafc562705520348678d
Melker <memleoberg@gmail.com>
2025-11-09 18:19:28 +0100

    auto commit [#41]

--- code/code/sandbox/main.c
+++ code/code/sandbox/main.c
@@ -4,12 +4,17 @@
 /*
  * assume i got a 8 by 16 display.
  * make some text move across it
- * */
-
+ *
+ **/
 int main() {
-  char text[] = "HIo";
+  char text[] = "HI";
   int textLenght = strlen(text);
   printf("%d", textLenght);
+
+  const int FONT_I[8] = {
+      0b10000001, 0b10000001, 0b10000001, 0b11111111,
+      0b10000001, 0b10000001, 0b10000001, 0b00000000,
+  };
 
   for (int i = 0; i < 8; i++) {
     for (int j = 0; j < 32; j++) {
