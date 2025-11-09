3a63278a386dcaff2f45d55c36954c2ac799a442
Melker <memleoberg@gmail.com>
2025-11-09 23:12:40 +0100

    auto commit [#44]

--- code/code/sandbox/main.c
+++ code/code/sandbox/main.c
@@ -9,6 +9,10 @@
 int main() {
   const int CHARACTER_WIDTH = 8;
   const int CHARACTER_HEIGHT = 8;
+  const int FONT_A[8] = {
+      0b01111100, 0b10000001, 0b10000001, 0b11111111,
+      0b10000001, 0b10000001, 0b10000001,
+  };
   const int FONT_H[8] = {
       0b10000001, 0b10000001, 0b10000001, 0b11111111,
       0b10000001, 0b10000001, 0b10000001, 0b00000000,
