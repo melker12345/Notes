695594a055dcec962df1218671d6793a40945b06
Melker <memleoberg@gmail.com>
2025-11-09 17:11:34 +0100

    auto commit [#27]

--- code/code/sandbox/main.c
+++ code/code/sandbox/main.c
@@ -6,8 +6,9 @@
  * */
 
 int main() {
-  for (int i = 0; i <= 8; i++) {
-    printf("*");
+  for (int i = 0; i <= 16; i++) {
+    for (int j = 0; j <= 8; j++)
+      printf("*");
   }
 
   return 0;
