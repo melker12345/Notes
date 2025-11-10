667f54665dae95493d3a806ae6564c2ee86c4874
Melker <memleoberg@gmail.com>
2025-11-10 11:28:12 +0100

    auto commit [#62]

--- code/code/sandbox/main.c
+++ code/code/sandbox/main.c
@@ -6,40 +6,48 @@
  * make some text move across it
  *
  **/
+const int FONT_A[8] = {
+    0b01111110, 0b10000001, 0b10000001, 0b10000001,
+    0b11111111, 0b10000001, 0b10000001, 0b10000001,
+};
+const int FONT_B[8] = {
+    0b11111110, 0b10000001, 0b10000001, 0b11111110,
+    0b10000001, 0b10000001, 0b10000001, 0b11111110,
+};
+const int FONT_H[8] = {
+    0b10000001, 0b10000001, 0b10000001, 0b11111111,
+    0b10000001, 0b10000001, 0b10000001, 0b00000000,
+};
+
+enum Font {
+  FONT_A_INDEX,
+  FONT_B_INDEX,
+  FONT_H_INDEX,
+};
+const *int fonts[FONT_COUNT]{FONT_A, FONT_B, FONT_H};
+
+void printFont() {
+  for (int y = 0; y < CHARACTER_HEIGHT; y++) {
+    int row_pattern = FONT_A[y];
+
+    for (int x = 0; x < CHARACTER_WIDTH; x++) {
+      int bit_mask = 1 << (CHARACTER_WIDTH - 1 - x);
+
+      if (row_pattern & bit_mask) {
+        printf("*");
+      } else {
+        printf(" ");
+      }
+    }
+    printf("\n");
+  }
+}
+
 int main() {
   const int CHARACTER_WIDTH = 8;
   const int CHARACTER_HEIGHT = 8;
-  const int FONT_A[8] = {
-      0b01111110, 0b10000001, 0b10000001, 0b10000001,
-      0b11111111, 0b10000001, 0b10000001, 0b10000001,
-  };
-  const int FONT_B[8] = {
-      0b11111110, 0b10000001, 0b10000001, 0b11111110,
-      0b10000001, 0b10000001, 0b10000001, 0b11111110,
-  };
-  const int FONT_H[8] = {
-      0b10000001, 0b10000001, 0b10000001, 0b11111111,
-      0b10000001, 0b10000001, 0b10000001, 0b00000000,
-  };
-
-  enum letters { FONT_A, 1 = FONT_B, 2 = FONT_H };
-
   // Move FONTS into a enum and then loop through all.
   for (int i = 0; i < 2; i++) {
-    for (int y = 0; y < CHARACTER_HEIGHT; y++) {
-      int row_pattern = FONT_A[y];
-
-      for (int x = 0; x < CHARACTER_WIDTH; x++) {
-        int bit_mask = 1 << (CHARACTER_WIDTH - 1 - x);
-
-        if (row_pattern & bit_mask) {
-          printf("*");
-        } else {
-          printf(" ");
-        }
-      }
-      printf("\n");
-    }
   }
 
   return 0;
